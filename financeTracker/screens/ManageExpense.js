8d665144732a500df4b6e5548d0e4095a0a1fcde
Stuart-Yee <ki6fzb@gmail.com>
2023-08-27 20:52:28 -0700

    initial init commit

---
+++ financeTracker/screens/ManageExpense.js
@@ -0,0 +1,14 @@
+import {Text, View} from "react-native"
+
+const ManageExpense = () => {
+
+    return (
+        <View>
+            <Text>
+                Manage Screen
+            </Text>
+        </View>
+    )
+}
+
+export default ManageExpense;
